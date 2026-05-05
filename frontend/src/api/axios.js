da24f40061e4f7561f9058254359ec1a76bcd975
Lakhbir singh <lakhbir9835singh@gmail.com>
2026-05-05 13:27:13 +0530

    Point frontend to remote backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:5000/api',
+  baseURL: import.meta.env.PROD ? 'https://task-manager-backend-chi-ashy.vercel.app/api' : 'http://localhost:5000/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
